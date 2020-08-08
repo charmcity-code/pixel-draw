36ef2cc69439468f74f8fe80d8adb8b93e0c0982
Mark Lawrence <lawrence.ml@gmail.com>
2020-08-08 15:04:25 -0400

    create pixel component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Pixel />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Pixel from "./components/Pixel";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Pixel
   }
 };
 </script>
